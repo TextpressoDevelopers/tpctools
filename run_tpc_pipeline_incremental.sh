c670ee7ea952df43380811b87dcacd3886607c7c
Valerio Arnaboldi <valearna@caltech.edu>
2018-08-30 08:29:44 -0700

    rebuild index at every update

--- run_tpc_pipeline_incremental.sh
+++ run_tpc_pipeline_incremental.sh
@@ -420,53 +420,46 @@ if [[ $(array_contains "${EXCLUDE_STEPS[@]}" "index") == "0" ]]
 then
     echo "Updating index ..."
     export INDEX_PATH=${INDEX_DIR}
-    if [[ ! -d ${INDEX_DIR} || $(ls ${INDEX_DIR} | grep -v "subindex_0" | wc -l) == "0" ]]
+    INDEX_DIR_CUR="${INDEX_DIR}"
+    if [[ -d ${INDEX_DIR} && $(ls ${INDEX_DIR} | grep -v "subindex_0" | wc -l) != "0" ]]
     then
-        mkdir -p "${INDEX_DIR}/db"
-        create_single_index.sh -m 100000 ${CAS2_DIR} ${INDEX_DIR}
-        cd "${INDEX_DIR}"
-        num_subidx_step=$(echo "${PAPERS_PER_SUBINDEX}/100000" | bc)
-        first_idx_in_master=0
-        final_counter=0
-        last_idx_in_master=${num_subidx_step}
-        num_subidx=$(ls | grep "subindex_" | wc -l)
-        found="0"
-        while [[ ${found} == "0" ]]
+        INDEX_DIR_CUR="${INDEX_DIR}_new"
+    fi
+    mkdir -p "${INDEX_DIR_CUR}/db"
+    create_single_index.sh -m 100000 ${CAS2_DIR} ${INDEX_DIR_CUR}
+    cd "${INDEX_DIR_CUR}"
+    num_subidx_step=$(echo "${PAPERS_PER_SUBINDEX}/100000" | bc)
+    first_idx_in_master=0
+    final_counter=0
+    last_idx_in_master=${num_subidx_step}
+    num_subidx=$(ls | grep "subindex_" | wc -l)
+    found="0"
+    while [[ ${found} == "0" ]]
+    do
+        if [[ ${last_idx_in_master} -ge ${num_subidx} ]]
+        then
+            last_idx_in_master=${num_subidx}
+            found="1"
+        fi
+        for ((i=$((first_idx_in_master + 1)); i<=$((last_idx_in_master-1)); i++))
         do
-            if [[ ${last_idx_in_master} -ge ${num_subidx} ]]
-            then
-                last_idx_in_master=${num_subidx}
-                found="1"
-            fi
-            for ((i=$((first_idx_in_master + 1)); i<=$((last_idx_in_master-1)); i++))
-            do
-                indexmerger subindex_${first_idx_in_master} subindex_${i} no
-                rm -rf subindex_${i}
-            done
-            if [[ "subindex_${first_idx_in_master}" != "subindex_${final_counter}" ]]
-            then
-                mv subindex_${first_idx_in_master} subindex_${final_counter}
-            fi
-            first_idx_in_master=$((first_idx_in_master + num_subidx_step))
-            last_idx_in_master=$((last_idx_in_master + num_subidx_step))
-            final_counter=$((final_counter + 1))
+            indexmerger subindex_${first_idx_in_master} subindex_${i} no
+            rm -rf subindex_${i}
         done
-        saveidstodb -i ${INDEX_DIR}
-        chmod -R 777 "${INDEX_DIR}/db"
-    else
-        ## pdf
-        templist=$(mktemp)
-        grep -v "Supplemental" ${newpdf_list} | awk -v cas2_dir="${CAS2_DIR}" -F"/" '{print cas2_dir"/C. elegans/"$(NF-1)}' | xargs -I {} find "{}" -name  *.tpcas.gz | awk -F "/" '{print $(NF-2)"/"$(NF-1)"/"$NF}' > ${templist}
-        # remove possible old versions and add new files for pdf documents
-        cas2index -i ${CAS2_DIR} -o ${INDEX_DIR} -a ${templist}
-        grep "Supplemental" ${newpdf_list} | awk -v cas2_dir="${CAS2_DIR}" -F"/" '{print cas2_dir"/C. elegans Supplementals/"$(NF-1)}' | xargs -I {} find "{}" -name *.tpcas.gz | awk -F "/" '{print $(NF-2)"/"$(NF-1)"/"$NF}' > ${templist}
-        # remove possible old versions and add new files for supplemental material
-        cas2index -i ${CAS2_DIR} -o ${INDEX_DIR} -a ${templist}
-        ## xml
-        awk -v cas2_dir="${CAS2_DIR}" -F"/" '{print cas2_dir"/PMCOA/"$NF}' ${newxml_local_list} | xargs -I {} find "{}" -name *.tpcas.gz | awk -F "/" '{print $(NF-2)"/"$(NF-1)"/"$NF}' > ${templist}
-        # remove possible old versions and add new files for xml documents
-        cas2index -i ${CAS2_DIR} -o ${INDEX_DIR} -a ${templist}
-        rm ${templist}
+        if [[ "subindex_${first_idx_in_master}" != "subindex_${final_counter}" ]]
+        then
+            mv subindex_${first_idx_in_master} subindex_${final_counter}
+        fi
+        first_idx_in_master=$((first_idx_in_master + num_subidx_step))
+        last_idx_in_master=$((last_idx_in_master + num_subidx_step))
+        final_counter=$((final_counter + 1))
+    done
+    saveidstodb -i ${INDEX_DIR_CUR}
+    chmod -R 777 "${INDEX_DIR_CUR}/db"
+    if [[ -d "${INDEX_DIR}_new" ]]
+    then
+        rm -rf ${INDEX_DIR}
+        mv ${INDEX_DIR_CUR} ${INDEX_DIR}
     fi
 fi
 
