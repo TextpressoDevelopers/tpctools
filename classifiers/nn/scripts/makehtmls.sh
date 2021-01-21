ca2314bd76a0b9c5e876c56f35f3592192ecfe8e
Hans-Michael Muller <mueller@caltech.edu>
2021-01-20 17:08:47 -0800

    Changed URL for NN results.

--- classifiers/nn/scripts/makehtmls.sh
+++ classifiers/nn/scripts/makehtmls.sh
@@ -14,7 +14,7 @@ do
     do
 	echo $line | cut -f 1,8,9 -d "/" | cut -f 1 -d "." | sed 's#/#XXX#' \
 	    | sed 's#/#">#' | sed 's#$#</a><br>#' \
-	    | sed 's#XXX#<a target=”_blank” href="http://tazendra.caltech.edu/~postgres/cgi-bin/paper_editor.cgi?curator_id=two324\&action=Search\&data_number=#' \
+            | sed 's#XXX#<a target="_blank" href="http://tazendra.caltech.edu/~azurebrd/cgi-bin/forms/paper_display.cgi?action=Search+!\&data_number=#'\
 	    | sed 's/^1 /LOW    /' \
 	    | sed 's/^2 /LOW    /' \
 	    | sed 's/^3 /LOW    /' \
