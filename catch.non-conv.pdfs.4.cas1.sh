b9cbf8dabeb9abc9230cab56341adfb5cc3dd82b
Hans-Michael <mueller@caltech.edu>
2021-02-08 12:33:41 -0800

    Added script for processing pdfs that cannot by article2cas.

--- catch.non-conv.pdfs.4.cas1.sh
+++ catch.non-conv.pdfs.4.cas1.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 function usage {
-    echo "this script converts pdfs into cas files."
+    echo "this script converts pdfs to txt to cas files."
     echo
     echo "usage: $(basename $0) [-pcPh]"
     echo "  -p --pdf-dir      directory where raw pdf files are"
@@ -15,7 +15,7 @@ function usage {
 PDF_DIR="/data/textpresso/raw_files/pdf"
 CAS1_DIR="/data/textpresso/tpcas-1"
 N_PROC=8
-LOCKFILE="/data/textpresso/tmp/03pdf2cas.lock"
+LOCKFILE="/data/textpresso/tmp/catch.non-conv.pdfs.4.cas1.lock"
 if [[ -f "${LOCKFILE}" ]]
 then
     echo $(basename $0) "is already running."
@@ -69,17 +69,26 @@ else
     do
 	echo "${folder}"
 	counter=0
-	rm -f /tmp/"${folder}".*.list
+	rm -f /tmp/"${folder}".*.nc.list
+	rm -rf /tmp/"${folder}".*.nc.dir
 	for i in "${folder}"/*
 	do
-	    if [[ $i -nt "${CAS1_DIR}/$i" ]]
+	    if [[ -z $(ls "${CAS1_DIR}/$i/"*.gz 2>/dev/null) ]]
 	    then
 		d=${i#"${folder}"/}
 		bin=$(($counter % $N_PROC))
-		echo $d >> "/tmp/${folder}.$bin.list"
+		echo $d >> "/tmp/${folder}.$bin.nc.list"
+		rsync -a "$i" "/tmp/${folder}.$bin.nc.dir"
 		counter=$(($counter+1))
 	    fi
 	done
+    done
+    find /tmp/*.nc.dir -name "*.pdf" -print0 | xargs -I % -0 -n 1 -P 8 sh -c "pdftotext \"%\"; rm -f \"%\""
+    find /tmp/*.nc.dir -name "*.txt" -print0 | xargs -I % -0 -n 1 -P 8 sh -c "sed -i 's/\f/\n/g' \"%\""
+    for i in '\x1' '\x2' '\x3' '\x4' '\x5' '\x6' '\x7' '\x8' '\xE' '\xF' \
+		   '\x10' '\x11' '\x12' '\x13' '\x14' '\x15' '\x16' '\x18' '\x19' '\x1A' '\x1D' '\x1F'
+    do
+	find /tmp/*.nc.dir -name "*.txt" -print0 | xargs -I % -0 -n 1 -P 8 sh -c "sed -i 's/$i//g' \"%\""  
     done
     # run article2cas, with CAS1_DIR as CWD
     cd ${CAS1_DIR}
@@ -87,16 +96,16 @@ else
     do
 	for ((j=0; j<${N_PROC}; j++))
 	do
-	    if [[ -f "/tmp/${folder}.$j.list" ]]
+	    if [[ -f "/tmp/${folder}.$j.nc.list" ]]
 	    then
-		articles2cas -i "${PDF_DIR}/${folder}" -l "/tmp/${folder}.$j.list" -t 1 -o "${folder}" -p &
+		articles2cas -i "/tmp/${folder}.$j.nc.dir" -l "/tmp/${folder}.$j.nc.list" -t 3 -o "${folder}" -p &
 	    fi
 	done
 	wait
 	# gzip all tpcas files
 	find "${folder}" -name "*tpcas" -print0 | xargs -0 -n 8 -P 8 pigz 2>/dev/null
-	rm -f /tmp/"${folder}".*.list
+	rm -f /tmp/"${folder}".*.nc.list
+	rm -rf /tmp/"${folder}".*.nc.dir
     done
-    catch.non-conv.pdfs.4.cas1.sh -p $PDF_DIR -c $CAS1_DIR -P $N_PROC
     rm ${LOCKFILE}
 fi
